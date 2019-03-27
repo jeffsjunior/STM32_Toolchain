095edab9cfbbcdd998bb4e85f1024ac8fb314380
descomplexando <jeffs.junior01@gmail.com>
2019-03-27 02:12:22 -0300

    teste

---
+++ STM32_Loader/arquivos/programa.c
@@ -0,0 +1,48 @@
+#include <stm32f10x.h>
+#include <stm32f10x_rcc.h>
+#include <stm32f10x_gpio.h>
+#include "time_stm32.h"
+
+uint32_t tWaitMs = 25;
+uint32_t previousMillis = 0;
+
+int main(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    //Enable Peripheral Clock
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+
+    //Configure Pins
+    GPIO_StructInit(&GPIO_InitStructure);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+    //Configure sysTick timer via time_stm32.h
+    Time_Init();
+    while (1)
+    {
+        static int ledval = 1;
+        uint32_t curentMillis = millis();
+
+        if(curentMillis-previousMillis >= tWaitMs){
+            previousMillis = curentMillis;
+            GPIO_WriteBit(GPIOC, GPIO_Pin_13, (ledval) ? Bit_SET : Bit_RESET);
+            ledval = 1 - ledval;
+        }
+    }
+    return 0;
+}
+
+void SysTick_HandlerT(void)
+{
+    
+}
+
+#ifdef USE_FULL_ASSERT
+void assert_failed(uint8_t *file, uint32_t line)
+{
+    while (1);
+}
+#endif
---
+++ STM32_Loader/arquivos/time_stm32.h
@@ -0,0 +1,31 @@
+file:///home/jefferson/stm32f1_stdper/myLibrary/time_stm32.h
+#ifndef _TIME_STM32_H
+#define _TIME_STM32_H
+
+volatile uint32_t milliseconds;
+volatile uint32_t microseconds;
+
+void Time_Init();
+void delay_ms(uint32_t time_ms);
+void delay_us(uint32_t time_us);
+
+static inline uint32_t millis(void){
+    return milliseconds;
+}
+static inline uint32_t micros(void)
+{
+    uint32_t temp_ms;
+    uint32_t tick_cnt;
+    uint32_t microseconds;
+
+     do{
+         tick_cnt = SysTick->VAL;
+         temp_ms = millis();
+     } while (temp_ms != millis());
+    //temp_ms = millis();
+    microseconds = temp_ms*1000 + (1000 - tick_cnt/72); 
+    return microseconds;
+}
+
+
+#endif
